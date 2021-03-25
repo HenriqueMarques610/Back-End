795bc472ddee4597b0876f4bc611552717302dd3
HenriqueMarques610 <73908117+HenriqueMarques610@users.noreply.github.com>
2021-03-25 18:20:45 +0000

    gdfshdfs

--- ficha3/ArrayUtils.js
+++ ficha3/ArrayUtils.js
@@ -34,6 +34,66 @@ var arrayUtils={
         var avg=sum/array.length;
         return avg;
     },
+    indexOf: function(array,value){
+        var sum=0
+        for (let i = 0; i < array.length; i++) {
+            if(value==array[i]){
+                return i;
+            }
+        }
+     
+        return -1;
+    },
+    subArray: function(array,startIndex,endIndex){
+        var a=[];
+        for (let i = startIndex; i <= endIndex; i++) {
+            a.push(array[i]);
+        }
+        return a;
+    },
+    isSameLength: function(array,otherArray){
+        return array.length==otherArray.length
+    },
+    reverse: function(array){
+        var r=[];
+        for (let i = array.length-1; i >= 0; i--) {
+            r.push(array[i]);
+        }
+        return r;
+    },
+    swap: function(array,index1,index2){
+        
+        //Obter valor que esta num determinado indice no array
+        var val1=array[index1];
+        var val2=array[index2];
+
+        //Alterar um determinado indice com outro valor
+        array[index1]=val2;
+        array[index2]=val1;
+
+        return array;
+    },
+    contains: function(array,value){
+        // var sum=0
+        // for (let i = 0; i < array.length; i++) {
+        //     if(value==array[i]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //Forma mais simplificada
+        return this.indexOf(array,value)!=-1
+    },
+    concat: function(array,otherArray){
+        var concatArray=array;
+        for (let i = 0; i < otherArray.length; i++) {
+            concatArray.push(otherArray[i])
+            
+        }
+        return concatArray;
+    },
+
 };
 
 module.exports=arrayUtils;
--- ficha3/app.js
+++ ficha3/app.js
@@ -30,8 +30,29 @@ var log = require("./log.js");
 //==========================================
 var arrayUtils=require("./ArrayUtils.js");
 var array=[12,4,6,88,9,0];
+var otherArray=[7,10];
 
 console.log("O array esta vazio? "+arrayUtils.isEmpty(array));
 console.log("O maximo do array é: "+arrayUtils.Max(array));
 console.log("O minimo do array é: "+arrayUtils.Min(array));
 console.log("A media do array é: "+arrayUtils.Average(array));
+
+var value=6;
+console.log("O indice do valor é: "+ value +" é:"+arrayUtils.indexOf(array,value));
+
+var subArray=arrayUtils.subArray(array,3,5);
+console.log("O sub array é: "+subArray)
+
+var sameSize= arrayUtils.isSameLength(array,otherArray)
+console.log("Os array sao do mesmo tamanho?: "+sameSize);
+
+var invertedArray=arrayUtils.reverse(array);
+console.log("O array invertido fica: "+invertedArray)
+
+var swappedArray=arrayUtils.swap(array,0,4);
+console.log("O array alterado é: "+swappedArray)
+
+console.log("O contem o valor "+value+"? "+ arrayUtils.contains(array,value));
+
+var concatArray=arrayUtils.concat(array,otherArray);
+console.log("O array concatenado é: "+concatArray);
