ac2fdcc69b8077a2ff16f66c701b4f68c2c80497
henriquemarques610 <henriquemarques610@gmail.com>
2021-06-08 16:11:28 +0100

    .

--- ficha10/controllers/personController.js
+++ ficha10/controllers/personController.js
@@ -17,33 +17,39 @@ exports.postPersons = function (req, res, next) {
         })
 }
 
-//COM QUERY
+//COM QUERY / FAZER IF SE TIVER NULL
 exports.deletePersons = function (req, res, next) {
-    Person.destroy({ where: { id: req.query.id } })
+    if(req.query.id==undefined){
+        res.send("User not found or doesn´t exist")
+    }
+    else{
+    Person.destroy({
+        where: { id: req.query.id }
+    }).then(result => {
+        res.send({ "Person Deleted with success.": result })
+    }).catch(err => {
+        console.error("User not added", err)
+    })
+}
+}
+
+//COM PARAMETROS 
+exports.getPersons1 = function (req, res, next) {
+    Person.findAll({
+        where: { id: req.params.id }
+    })
         .then(result => {
-            res.send({ "Person Deleted with success.": result })
+            res.send({ "Person: ": result })
         }).catch(err => {
-            console.error("User not added", err)
+            console.error("No user found", err)
         })
 }
 
-//COM PARAMETROS / FAZER IF SE TIVER NULL
-exports.getPersons1 = function (req, res, next) {
-    Person.findAll({
-        where:{id:req.params.id}
-    })
-    .then(result=>{
-        res.send({"Person: ":result})
-    }).catch(err => {
-        console.error("No user found", err)
-    })
-}
-
 exports.putPersons = function (req, res, next) {
-    var details=req.body
+    var details = req.body
     Person.update(details,
         {
-            where:{id:req.params.id}
+            where: { id: req.params.id }
         })
         .then(result => {
             res.send({ "Person Updated with success.": result })
